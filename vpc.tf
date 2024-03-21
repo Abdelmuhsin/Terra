58e6828b5688f4866586bc238acda0988de280fb
Abdelmuhsin <abdo.code1997@gmail.com>
2024-03-21 21:20:37 +0530

    create subnets

--- vpc.tf
+++ vpc.tf
@@ -7,3 +7,33 @@ resource "aws_vpc" "pr-vpc" {
     Name = "pr-vpc"
   }
 }
+
+# web subnet
+resource "aws_subnet" "web-sub" {
+  vpc_id     = aws_vpc.pr-vpc.id
+  cidr_block = "10.0.1.0/16"
+
+  tags = {
+    Name = "web-sub"
+  }
+}
+
+# api subnet
+resource "aws_subnet" "api-sub" {
+  vpc_id     = aws_vpc.pr-vpc.id
+  cidr_block = "10.0.2.0/16"
+
+  tags = {
+    Name = "api-sub"
+  }
+}
+
+# db subnet
+resource "aws_subnet" "db-sub" {
+  vpc_id     = aws_vpc.pr-vpc.id
+  cidr_block = "10.0.3.0/16"
+
+  tags = {
+    Name = "db-sub"
+  }
+}
