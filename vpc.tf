cc93eae6b52ad7ace36ae104e7dbae3540da7269
Abdelmuhsin <abdo.code1997@gmail.com>
2024-03-21 21:22:50 +0530

    create subnets

--- vpc.tf
+++ vpc.tf
@@ -11,7 +11,7 @@ resource "aws_vpc" "pr-vpc" {
 # web subnet
 resource "aws_subnet" "web-sub" {
   vpc_id     = aws_vpc.pr-vpc.id
-  cidr_block = "10.0.1.0/16"
+  cidr_block = "10.0.0.0/16"
 
   tags = {
     Name = "web-sub"
@@ -21,7 +21,7 @@ resource "aws_subnet" "web-sub" {
 # api subnet
 resource "aws_subnet" "api-sub" {
   vpc_id     = aws_vpc.pr-vpc.id
-  cidr_block = "10.0.2.0/16"
+  cidr_block = "10.0.0.0/16"
 
   tags = {
     Name = "api-sub"
@@ -31,7 +31,7 @@ resource "aws_subnet" "api-sub" {
 # db subnet
 resource "aws_subnet" "db-sub" {
   vpc_id     = aws_vpc.pr-vpc.id
-  cidr_block = "10.0.3.0/16"
+  cidr_block = "10.0.0.0/16"
 
   tags = {
     Name = "db-sub"
