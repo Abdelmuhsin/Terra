2a5e771cd04ba712dcb2fa16dd917ec403a80dea
Abdelmuhsin <abdo.code1997@gmail.com>
2024-03-21 23:54:15 +0530

    ch

--- vpc.tf
+++ vpc.tf
@@ -220,7 +220,7 @@ resource "aws_vpc_security_group_ingress_rule" "web-sg-http" {
 
 #web security group egress 
 resource "aws_vpc_security_group_egress_rule" "web-sg-egress" {
-  security_group_id = aws_security_group.allow_tls.id
+  security_group_id = aws_security_group.web-sg.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
