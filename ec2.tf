804708ba62be82842a6b799ca1f43b83510b09bc
Abdelmuhsin <abdo.code1997@gmail.com>
2024-03-22 21:30:44 +0530

    ch

--- ec2.tf
+++ ec2.tf
@@ -1,13 +1,12 @@
 # EC2 web server
 resource "aws_instance" "pr-instance" {
-  instance_type = "t2.micro"
+  instance_type          = "t2.micro"
   ami                    = "ami-019f9b3318b7155c5"
-  key_name               = abd
+  key_name               = "abd"  # Enclose the key pair name in double quotes
   subnet_id              = aws_subnet.web-sub.id
   vpc_security_group_ids = [aws_security_group.web-sg.id]
-  user_data = file("setup.sh")
-   tags = {
+  user_data              = file("setup.sh")
+  tags = {
     Name = "pr-instance"
   }
 }
-  
