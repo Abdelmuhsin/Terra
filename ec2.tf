60a329c039a6aa0ca6304de70012ad459f14f679
Abdelmuhsin <abdo.code1997@gmail.com>
2024-03-22 21:26:44 +0530

    ch

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,9 @@
 # EC2 web server
 resource "aws_instance" "pr-instance" {
-  ami           = "ami-01387af90a62e3c01"
   instance_type = "t2.micro"
-  subnet_id      = aws_subnet.api-sub.id
-  key_name = "pr"
+  ami                    = "ami-019f9b3318b7155c5"
+  key_name               = abd
+  subnet_id              = aws_subnet.web-sub.id
   vpc_security_group_ids = [aws_security_group.web-sg.id]
   user_data = file("setup.sh")
    tags = {
--- vpc.tf
+++ vpc.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "pr-vpc" {
 }
 
 # web subnet
-resource "aws_subnet" "web-sub" {
+resource "aws_subnet" "" {
   vpc_id     = aws_vpc.pr-vpc.id
   cidr_block = "10.0.1.0/24"
    map_public_ip_on_launch = "true"
